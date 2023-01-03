13ab602974898ed73b3939cda6a7a3b4542720f7
kilogold <bonilla_kelvin@hotmail.com>
2023-01-03 14:30:24 -0800

    Fixing Ganache back to sensible dev defaults. Facilitating automated testing & endless confirmations by gas configs.

--- dev-ganache-cli.bat
+++ dev-ganache-cli.bat
@@ -1,1 +1,1 @@
-ganache --fork=sepolia --blockTime 6 --wallet.accounts="%POS_ADMIN_PRIV_KEY%,1000000000000000000000" "%POS_USER_PRIV_KEY%,1000000000000000000000"
+ganache --chain.vmErrorsOnRPCResponse true --wallet.accounts "%POS_ADMIN_PRIV_KEY%,1000000000000000000000" "%POS_USER_PRIV_KEY%,1000000000000000000000" --chain.hardfork istanbul -b 6
