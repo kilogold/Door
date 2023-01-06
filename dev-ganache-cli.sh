f2bbeb43928e0299e89870121f0445a7b27848ee
Kelvin Bonilla <bonilla_kelvin@hotmail.com>
2023-01-05 18:45:13 -0800

    Parity with Windows script

--- dev-ganache-cli.sh
+++ dev-ganache-cli.sh
@@ -1,1 +1,1 @@
-ganache-cli --blockTime 6 --account="$POS_ADMIN_PRIV_KEY,1000000000000000000000" --account="$POS_USER_PRIV_KEY,1000000000000000000000" --chain.hardfork istanbul --fork.network sepolia
+ganache-cli --blockTime 6 --account="$POS_ADMIN_PRIV_KEY,1000000000000000000000" --account="$POS_USER_PRIV_KEY,1000000000000000000000" --fork.network sepolia
